8a0c652c6fb0dbf87297885c2aac9b75028fa70c
Mohan Kumar T <mmohan8617@gmail.com>
2025-12-04 20:29:59 +0530

    Modified program.py

--- kmeans/program.py
+++ kmeans/program.py
@@ -2,10 +2,12 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 
-x, y = make_blobs(n_samples=500, centers=4, random_state=42)
+X, y = make_blobs(n_samples=500, centers=4, random_state=42)
+
+
 kmean = KMeans(n_clusters=4, random_state=42)
-kmean.fit(x)
-predicted_labels = kmean.predict(x)
-plt.scatter(x[:,0], x[:,1], c=predicted_labels)
+kmean.fit(X)
+predicted_labels = kmean.predict(X)
+plt.scatter(X[:,0], X[:,1], c=predicted_labels)
 plt.title("K-Means clustering")
 plt.show()
